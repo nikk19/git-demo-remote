218b1d172e9f74c7b42216c110ac33b9c7160d0e
nikk19 <54307981+nikk19@users.noreply.github.com>
2022-03-08 17:51:30 +0530

    initial commit on math.py

---
+++ calc.py
@@ -0,0 +1,15 @@
+def addition(a,b,c):
+    pass
+
+def mul_add(a,b,c):
+    return a*b + c  #On master branch
+    
+def xor(a,b):
+    return a^b  #Added implementation on coolFeature branch
+    
+#func implementation is completed
+def square(x):
+    return x*x
+    
+def cube(x):
+    return x*x*x
